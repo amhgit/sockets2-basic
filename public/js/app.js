fbee84f0503d16c71d002671d871a46c8678c056
Andres Hidalgo <ahidalgo@verizon.net>
2016-03-23 17:20:24 -0500

    95: Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,26 @@
- var socket = io();
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
 
- socket.on('connect', function() {
+var socket = io();
+
+console.log(name + ' wants to join ' + room);
+
+socket.on('connect', function() {
 	console.log('Connected to socket.io server!');
- });
+});
 
- socket.on('message', function(message) {
+socket.on('message', function(message) {
  	console.log('New message:');
  	console.log(message.text);
     var timestamp = moment.utc(message.timestamp);
  
  	//jQuery('.messages').append('<p>' + message.text + '</p');
  	//jQuery('.messages').append('<p>' + timestr + '</p');
-    
- 	jQuery('.messages').append('<p><strong>' + timestamp.local().format('h:mm a') + ':</strong> ' + message.text + '</p');
+    $message = jQuery('.messages');
+    $message.append('<p><strong>' + message.name + ' ' + timestamp.local().format('h:mm a') + '</strong></p');
+    $message.append('<p>' +message.text + '</p>');
      
- });
+});
 
 // handles submitting a new message
 var $form = jQuery('#message-form');
@@ -22,7 +28,9 @@ var $form = jQuery('#message-form');
 $form.on('submit', function(event){
     event.preventDefault(); // will messagehandle submission ourselves
     
-    socket.emit('message',{text:         $form.find('input[name=message]').val()                   
+    socket.emit('message',{
+        name: name,
+        text: $form.find('input[name=message]').val()                   
     });
     
     //clear the inut box
