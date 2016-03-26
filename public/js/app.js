fbc077ddfb7ee1ec5468b4787df720882a9e9791
Andres Hidalgo <ahidalgo@verizon.net>
2016-03-26 14:11:21 -0500

    98: Add support for chat room

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,20 @@
 var name = getQueryVariable('name') || 'Anonymous';
 var room = getQueryVariable('room');
-
 var socket = io();
 
 console.log(name + ' wants to join ' + room);
+jQuery('.room-title').text(room);
 
 socket.on('connect', function() {
 	console.log('Connected to socket.io server!');
+ 
+    socket.emit('joinRoom', {
+        name: name,
+        room: room
+    });
 });
 
-socket.on('message', function(message) {
+socket.on('message', function(message) { 
  	console.log('New message:');
  	console.log(message.text);
     var timestamp = moment.utc(message.timestamp);
@@ -17,12 +22,11 @@ socket.on('message', function(message) {
  	//jQuery('.messages').append('<p>' + message.text + '</p');
  	//jQuery('.messages').append('<p>' + timestr + '</p');
     $message = jQuery('.messages');
-    $message.append('<p><strong>' + message.name + ' ' + timestamp.local().format('h:mm a') + '</strong></p');
-    $message.append('<p>' +message.text + '</p>');
-     
+    $message.append('<p><strong>' + message.name + ' ' + timestamp.local().format('h:mm a') + '</strong></p>');
+    $message.append('<p>' + message.text + '</p>');     
 });
 
-// handles submitting a new message
+// handles submitting a new message 
 var $form = jQuery('#message-form');
 
 $form.on('submit', function(event){
--- server.js
+++ server.js
@@ -7,25 +7,36 @@ var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 
+var clientInfo = {};
+
 io.on('connection', function(socket) {
    console.log('User connected via socket.io!'); 
-
+    
+    socket.on('joinRoom',function(req) {
+        clientInfo[socket.id] = req;
+        socket.join(req.room);
+        socket.broadcast.to(req.room).emit('message', {
+           name: 'System',
+            text: req.name + ' has joined!',
+            timestamp: moment().valueOf()
+        });
+    });
+    
    socket.on('message', function(message) {
        console.log('Message received: ' + message.text);
        // send it to everyone but me
        //socket.broadcast.emit('message', message);
        
-       
        // add timestamp
        message.timestamp = moment().valueOf(); //milliseconds
        // send it to everyone and me
-       io.emit( 'message', message);
-   })
+       io.to( clientInfo[socket.id].room).emit('message', message);
+   });
     
     socket.emit('message', {
         name: 'System',
-   	text: 'Welcome to the chat application!',
-    timestamp: moment().valueOf() 
+   	    text: 'Welcome to the chat application!',
+        timestamp: moment().valueOf() 
    });
 });
 
